5c5bec022e031bb9b5ee05eec61c8243082e6500
Carter Auer <89316179+JollyOmnivore@users.noreply.github.com>
2024-02-28 12:42:29 -0700

    Update Drone.lua
    Upgraded to have more capability's

--- Drone.lua
+++ Drone.lua
@@ -1,10 +1,11 @@
+turtle.refuel()  
 rednet.open("left")
  
 while(true) do
     id, msg, pro = rednet.receive()
     print(msg) 
     rednet.broadcast("received " .. msg .. " comand") 
-    if(msg == "w") then
+    if(msg =="w") then
         turtle.forward()
     elseif(msg == "s") then 
         turtle.back()
@@ -12,7 +13,7 @@ while(true) do
         turtle.turnLeft()
     elseif(msg == "d") then
         turtle.turnRight()
-    elseif(msg ==  "dig") then
+    elseif(msg == "dig") then
         turtle.dig()
     elseif(msg == "up")then
         turtle.up()
@@ -23,10 +24,23 @@ while(true) do
         has_block, data = turtle.inspect()
         if has_block then
             print(textutils.serialise(data.name))
-            rednet.broadcast("inspected" .. textutils.serialise(data.name))  
+            rednet.broadcast(textutils.serialise(data.name))  
         else 
             rednet.broadcast("No Block Detected")
         end
+    elseif(msg == "Hello") then 
+        os.setComputerLabel("Hello")  
+    elseif(msg == "digUp") then 
+        turtle.digUp()
+    elseif(msg == "digDown") then
+        turtle.digDown()
+    elseif(msg == "reFuel") then
+        turtle.refuel(1)
+    elseif(msg == "attack") then
+        turtle.attack()
+    elseif(msg == "clearlabel") then
+        os.setComputerLabel("")
+        
     end
     
     
